eb56bce545b681dc6300dafffb5f6fc9c0a109e8
testlins <276793413@163.com>
2013-04-11 16:09:52 +0800

    增加按时间先后执行功能
    1

--- sqluser/usesql_v0.3.py
+++ sqluser/usesql_v0.3.py
@@ -1,8 +1,10 @@
-# -*- coding: gbk -*-
+# -*- coding:gbk -*-
 #脚本功能，连接指定数据库；
 #扫描同文件夹下的.sql文件,读入并执行。
 #增加异常处理，日志记录功能
+#按时间升序 顺序执行
 #author：linxs
+#version v0.3
 # -*- coding:gbk -*-
 import pyodbc
 import os
@@ -22,22 +24,37 @@ try:
 except pyodbc.Error, dberror:
     mylog.error("数据库连接失败")
     mylog.error(dberror)
-#取sql中的文件名，循环打开文件 执行
 #此版本values值有两行，会报错hy000
+path = os.getcwd()+os.sep+"sql"
 sqldir = os.listdir(os.getcwd()+os.sep+"sql")
-print sqldir
-for file in sqldir:
-    if file[-4:]=='.sql':
-        fileopen = open(os.getcwd()+os.sep+"sql"+os.sep+file, "r+")
-        sqldo = fileopen.read()
-        try:
-            cursor1.execute(sqldo)
-            cursor1.commit()
-            mylog.info(file+' 执行成功')
-            fileopen.close()
-        except pyodbc.Error, dberror:
-            mylog.error(file+' 执行失败，请检查语句')
-            mylog.error(dberror)
+
+#取出sql文件最后修改时间，并排序
+
+files = []
+#for file in sqldir:
+#    if file[-4:]=='.sql':
+#        fileName = os.path.join(path,file)  
+#        filetime = os.stat(fileName).st_mtime
+ #       files.append((filetime,fileName))
+
+#网上的例子是函式编程 学习下
+files = [(os.path.getmtime(path+os.sep+file),os.path.join(path,file))for file in sqldir if file[-4:]=='.sql']
+files.sort()
+#循环执行sql文件
+for filex in files:
+    fileopen = open(filex[1], "r+")
+    sqldo = fileopen.read()
+
+#sqldo = [open(filex[1],"r+").read() for filex in files]
+#print sqldo    
+    try:
+        cursor1.execute(sqldo)
+        cursor1.commit()
+        mylog.info((os.path.split(filex[1]))[1]+' 执行成功')
+        fileopen.close()
+    except pyodbc.Error, dberror:
+        mylog.error((os.path.split(filex[1]))[1]+' 执行失败，请检查语句')
+        mylog.error(dberror)
 cursor1.close()
 
 
