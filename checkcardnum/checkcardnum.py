0a2f193b7290a117de069da6485d8bc25ea97c6c
testlins <276793413@163.com>
2013-04-11 23:13:02 +0800

    增加年龄计算

--- checkcardnum/checkcardnum.py
+++ checkcardnum/checkcardnum.py
@@ -3,10 +3,12 @@
 #先做位数校验和出生年月日校验
 #先用正则判断是否为合法日期
 #在判断日期是否在startdate和目前时刻之间
+#增加年龄计算
 import os
 import time
 import re
-import checkdate
+#import checkdate
+import calc_age
 def cardcheck(cardnum):
     #身份证最后一位包含数字
     values =['1','0','X','9','8','7','6','5','4','3','2']
@@ -15,7 +17,7 @@ def cardcheck(cardnum):
     if len(cardnum)==18 and cardnum[0:17].isdigit() and cardnum[-1] in values:
         birdate = cardnum[6:14]
         #调用checkdate检查日期合法性
-        if checkdate.checkdate(birdate):
+        if calc_age.calc_age(birdate):
             #最后一位校验码规则:
             """
             将身份证号码分别乘以不同的系数。从第1位到第17位的系数分别为：7 9 10 5 8 4 2 1 6 3 7 9 10 5 8 4 2 1
@@ -30,7 +32,8 @@ def cardcheck(cardnum):
             sumnub = sum(multip)
 
             if values[sumnub%11]==cardnum[-1]:
-                print '1'
+                print "CardId is true"
+                print "The people age is %s"%calc_age.calc_age(birdate)
                 return True
             else:
                 return False               
