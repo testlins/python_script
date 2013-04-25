2c3eb94aeff4d10a3791dca5988db2e1f4c5a5a4
testlins <276793413@163.com>
2013-04-25 10:46:58 +0800

    增加打开svnlog功能
    1

--- copyfile/copytjb.py
+++ copyfile/copytjb.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+#author=linxs
 from win32com.shell import shell, shellcon
 import os
 import filecmp
@@ -14,25 +15,27 @@ with open('copytjb.ini','r+') as cfgfile:
     config.readfp(cfgfile)
     olddir = config.get('dirinfo','olddir')
     newdir = config.get('dirinfo','newdir')
-    scofile = config.get('dirinfo','scofile')
-    localfile = config.get('dirinfo','localfile')
+    reportfile = config.get('dirinfo','reportfile')
+    localreportfile = config.get('dirinfo','localreportfile')
     #win平台下编码成utf-8
     lookup = (config.get('filter','lookup')).decode('gbk').encode('utf-8')
     sleeptime = config.get('time','sleeptime')
-    killpro = config.get('process','killpro')
+    #killpro = config.get('process','killpro')
     processname = config.get('process','processname')
-
+    svnfile = config.get('dirinfo','svnfile')
+    localsvnfile = config.get('dirinfo','localsvnfile')
+    opensvnfile = config.get('opensvn','opensvnfile')
     
-def ischange(scofile,localfile):
+def ischange(reportfile,localreportfile):
     """判断report文件是否有改变"""
-    if not os.path.exists(localfile):
-        localfile = file(localfile,'w')
-        localfile.close()
+    if not os.path.exists(localreportfile):
+        localreportfile = file(localreportfile,'w')
+        localreportfile.close()
         mylog.info('文件创建成功')
         return True
         
     else:
-        if not filecmp.cmp(scofile,localfile):
+        if not filecmp.cmp(reportfile,localreportfile):
             mylog.info('report对比有改变')
             return True
         else:
@@ -54,35 +57,41 @@ def findstrinfile(filename, lookup):
 
 def kill(processname):
     """杀进程，后面学习其他系统监控方法"""
-    if killpro == 'True':
-        command = 'taskkill /F /IM %s'%processname
-        os.system(command)
-        mylog.info('程序已关闭/未打开')
-    else:
+    #if killpro == 'True':
+    command = 'taskkill /F /IM %s'%processname
+    os.system(command)
+    mylog.info('程序已关闭/未打开')
+    #else:
         #print '酱油'
-        pass
+        #pass
     
 
 def copytjb():
     """拷文件"""
-    mylog.info('准备复制olddir，scofile')
+    mylog.info('准备复制olddir，reportfile')
     try:
         shell.SHFileOperation ((0, shellcon.FO_COPY, olddir, newdir,shellcon.FOF_NOCONFIRMMKDIR|shellcon.FOF_NOCONFIRMATION, None, None))
-        shell.SHFileOperation ((0, shellcon.FO_COPY, scofile, localfile,shellcon.FOF_NOCONFIRMATION, None, None))
-        mylog.info('复制olddir，scofile成功')
+        shell.SHFileOperation ((0, shellcon.FO_COPY, reportfile, localreportfile,shellcon.FOF_NOCONFIRMATION, None, None))
+        shell.SHFileOperation ((0, shellcon.FO_COPY, svnfile, localsvnfile,shellcon.FOF_NOCONFIRMATION, None, None))
+        mylog.info('复制olddir，reportfile成功')
     except:
         mylog.error('复制失败，请关闭相关程序，再试')
 
-#def opentjb():
-#    command = 'C:\Users\casking_lxs\Desktop\client12\H365.TJB.Client.exe'
-#    os.system(command)   
+def opensvnlog():
+    if opensvnfile == 'True':
+        command = 'c:/windows/system32/notepad.exe  %s'%localsvnfile
+        os.system(command)
+    else:
+        pass
+  
 
 def copy():
     kill(processname)
     copytjb()
+    opensvnlog()
 if __name__ == '__main__':
     while True:
-        if ischange(scofile,localfile) and findstrinfile(scofile,lookup):
+        if ischange(reportfile,localreportfile) and findstrinfile(reportfile,lookup):
             root = Tkinter.Tk()
             label =Tkinter.Label(root,text='程序已有更新').pack()
             button_sure = Tkinter.Button(root,text='更新',command = copy)
